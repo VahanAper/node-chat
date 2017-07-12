d63db62a64b58ac16581dbbaef7536f680a895ff
VahanAper <asryanvahandev@gmail.com>
2017-07-12 16:20:39 +0400

    init message form and messages list

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,19 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New Message.', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi...',
-}, function(data) {
-  console.log(data);
+jQuery('#message-form').on('submit', function(event) {
+  event.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val(),
+  }, function() {
+    console.log('Acknolidged');
+  });
 });
